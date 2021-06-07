ff0c73c9c9d024167721b64ba93b2e1af349ebe1
Mohamed Ramadan <m7mdrama1245@gmail.com>
2021-06-07 22:34:01 +0200

    Create countries & cities | tables & seeder

---
+++ database/seeders/CountrySeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\City;
+use App\Models\Country;
+use Illuminate\Database\Seeder;
+
+class CountrySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Country::factory(10)->create()
+            ->each(function($country){
+            $country->cities()->saveMany(City:: factory( mt_rand(1,10) )
+            ->make());
+        });
+    }
+}

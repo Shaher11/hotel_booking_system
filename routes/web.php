a5c1bb4745c82bdd71573c1371265dec7a48511a
Mohamed Ramadan <m7mdrama1245@gmail.com>
2021-06-10 03:43:16 +0200

    Add DB facedes query to retrive the available rooms in spesific date

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Reservation;
+use App\Models\Room;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +17,41 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    $check_in = '2021-6-16';
+    $check_out = '2021-6-19';
+
+    // $result = Reservation::where(function($q) use($check_in, $check_out) {
+    //     $q->where('check_in', '>', $check_in);
+    //     $q->where('check_in', '>=', $check_out);
+        
+    // })->orWhere(function($q) use($check_in, $check_out) {
+    //     $q->where('check_out', '<=', $check_in);
+    //     $q->where('check_out', '<', $check_out);
+        
+    // })->get(); //  To return empty array
+    
+
+   $result = DB::table('rooms')->whereNotExists(function ($query) use ($check_in, $check_out) {
+        
+         $query->select('reservations.id')
+                ->from('reservations')
+                ->join('reservation_room', 'reservations.id', '=', 'reservation_room.reservation_id')
+                ->whereRaw('rooms.id = reservation_room.room_id')
+                ->where(function($q) use($check_in, $check_out) {
+            
+                $q->where('check_out', '>', $check_in);
+                $q->where('check_in', '<', $check_out);
+                
+            })->limit(1);    
+        })->paginate(10);
+        
+    
+
+        
+    
+    dump($result);
+    
+    
     return view('welcome');
 });
